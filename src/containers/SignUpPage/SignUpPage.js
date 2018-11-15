d50c3966ab275a720adb620c462888e41a09a17e
Jangwon <wkddnjset@github.com>
2018-11-16 03:20:52 +0900

    회원가입 폼 이벤트 개발

--- src/containers/SignUpPage/SignUpPage.js
+++ src/containers/SignUpPage/SignUpPage.js
@@ -7,10 +7,24 @@ import logo from '../../images/favicon.png';
 import './style.css'
 
 class SignUpPage extends Component {
-	
+	// state 설정
+	constructor(){
+	    super()
+	    
+	    this.state = {
+	      // 이메일, 비밀번호
+	      email: null,
+	      password: null,
+	      password2:null
+	    }
+	    this.onChange = this.onChange.bind(this)
+	    this.SignInsubmit = this.SignInsubmit.bind(this)
+  	}
 	// 회원가입 구현!
 	SignUpsubmit(e){
 		alert(`
+			ID : ${this.state.email}
+			PW : ${this.state.password}
 			TODO : 회원가입 기능 구현
 			TODO : 회원가입 후 로그인 페이지로 이동`)
 		e.preventDefault()
@@ -19,6 +33,22 @@ class SignUpPage extends Component {
 	// 로그인 페이지로 이동
 	toSignin(){
 		this.props.history.push('/signin')
+	}
+
+	// Input Text 입력시
+	onChange(e){
+		// console.log(e.target.id)
+		// console.log(this)
+		// ID 값을 기준으로 State 변경
+		if (e.target.id==='EmailInput') {
+			this.setState({ email : e.target.value})
+		}
+		else if (e.target.id==='PasswordInput') {
+			this.setState({ password : e.target.value})
+		}
+		else if (e.target.id==='PasswordInput2') {
+			this.setState({ password2 : e.target.value})
+		}
 	}
   	render() {
     	return (
@@ -30,16 +60,19 @@ class SignUpPage extends Component {
 						id="EmailInput"
 						type="text"
 						placeholder="Email address"
+						onChange={this.onChange}
 					/>
 					<FieldGroup
 						id="PasswordInput"
 						type="password"
 						placeholder="Password"
+						onChange={this.onChange}
 					/>
 					<FieldGroup
 						id="PasswordInput2"
 						type="password"
 						placeholder="Password Confirm"
+						onChange={this.onChange}
 					/>
 					<Button bsStyle="secondary" onClick={() => this.toSignin()}>
 						Go Back to Sign in
--- src/utils/MediaHandler.js
+++ src/utils/MediaHandler.js
@@ -1,7 +1,7 @@
 export default class MediaHandler {
 	getPermissions() {
 		return new Promise((res, rej) => {
-			navigator.mediaDevices.getUserMedia({video: true, audio:  { facingMode: "user" }})
+			navigator.mediaDevices.getUserMedia({video: true, audio: true})
 				.then((stream) => {
 					res(stream)
 				})
